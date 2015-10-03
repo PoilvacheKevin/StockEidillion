4106ed55ae2c40f33f6bf08f728cc050b8fa1584
Niccis <poilvachekevin@gmail.com>
2015-10-03 15:34:18 +0200

    update

--- GestionStockEidillion/Accueil.vb
+++ GestionStockEidillion/Accueil.vb
@@ -1,12 +1,15 @@
 ﻿Imports MySql.Data.MySqlClient
 Public Class Form_Acceuil
-
     Private Connexion As New MySqlConnection
+
     Private Sub BT_Valider_Click(sender As Object, e As EventArgs) Handles BT_Valider.Click
         Dim Test As Boolean
         Test = Connect()
         If Test = True Then
-            Form_Stocks.Show()
+            Test = ID_Confirm()
+            If Test = True Then
+                Form_Stocks.Show()
+            End If
         Else
             TB_Password.Text = ""
             TB_User.Text = ""
@@ -14,7 +17,7 @@ Public Class Form_Acceuil
     End Sub
 
     Private Function Connect() As Boolean
-        Dim cs As String = "Database = EidillionStock;Data Source=127.0.0.1;" & "User Id=" & TB_User.Text & ";Password=" & TB_Password.Text & ";"
+        Dim cs As String = "Database = EidillionStock;Data Source=127.0.0.1; User Id=root;Password=;"
         Dim Connexion As New MySqlConnection(cs)
         Try
             Connexion.Open()
@@ -24,7 +27,41 @@ Public Class Form_Acceuil
             MsgBox(ex.Message)
             Return False
         End Try
+    End Function
+
+    Private Function ID_Confirm() As Boolean
+        Dim Naming(50) As String
+        Dim Password(50) As String
+        Dim i As Integer
+        Dim Request As String = "SELECT * FROM t_acces"
+        Dim Command As New MySqlCommand(Request, Connexion)
+        Dim Reader As MySqlDataReader
+        Reader = Command.ExecuteReader()
+
+        While Reader.Read()
+            Naming(i) = Reader.GetString(1)
+            Password(i) = Reader.GetString(2)
+            i += 1
+        End While
+
+        Reader.Close()
         Connexion.Close()
+        Dim Compare As Integer
+        Dim x As Integer
+
+        While Compare <> 0 And x < i
+            Compare = String.Compare(Naming(x), TB_User.Text)
+            Compare = String.Compare(Password(x), TB_Password.Text)
+            x += 1
+        End While
+
+        If Compare = 0 Then
+            Return True
+        Else
+            MessageBox.Show("Erreur Mdp ou Pseudonyme !")
+            Return False
+        End If
+
     End Function
 
 End Class
